4296330f46f7a086829b603469c1fbb2a5525bb9
harshawije-dev <37575752+harshawije97@users.noreply.github.com>
2025-08-04 18:23:27 +0530

    Update the sidebar

--- src/app/(home)/_components/main-sidebar.tsx
+++ src/app/(home)/_components/main-sidebar.tsx
@@ -6,11 +6,16 @@ import React, { ComponentRef } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 function MainSidebar() {
-  
-
   return (
     <>
-      
+      <aside className="w-60 h-[100vh] flex overflow-y-auto z-[99999] relative flex-col group/sidebar bg-secondary">
+        <div>
+          <p>Actions</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+      </aside>
     </>
   );
 }
